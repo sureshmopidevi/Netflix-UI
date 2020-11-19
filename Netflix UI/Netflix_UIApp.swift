33f37f596a06fdbe88dd528f968c0696836e828a
Suresh Mopidevi <suresh@jukshio.com>
2020-11-19 11:18:38 +0530

    Initial Commit

---
+++ Netflix UI/Netflix_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_UIApp.swift
+//  Netflix UI
+//
+//  Created by Sukshi  on 19/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
